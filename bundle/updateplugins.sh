879d9d4f456567c38c3da4e3f6f21744bbed9a16
Stefan Otte <stefan.otte@gmail.com>
2011-03-29 13:15:56 +0200

    less verbose updateplugins.sh.

--- bundle/updateplugins.sh
+++ bundle/updateplugins.sh
@@ -5,14 +5,10 @@ for dir in *
 do
   if [ -d "$dir" ]; then
     echo Updating $dir:
-    echo cd $dir
     cd $dir
-    echo git pull
     git pull
-    echo cd ..
     cd ..
     echo
-    #yay, we get matches!
   fi
 done
 
